683ba651c14b588cdb1e08d77a6fec702e5692d5
gksdud1109 <vectorh532@gmail.com>
2025-10-15 15:43:33 +0900

    feat: 유저 챌린지 참가, 탈퇴 기능 컨트롤러-서비스 로직 구현

--- motionit/src/main/java/com/back/motionit/domain/challenge/room/repository/ChallengeRoomRepository.java
+++ motionit/src/main/java/com/back/motionit/domain/challenge/room/repository/ChallengeRoomRepository.java
@@ -1,8 +1,20 @@
 package com.back.motionit.domain.challenge.room.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.back.motionit.domain.challenge.room.entity.ChallengeRoom;
 
+import jakarta.persistence.LockModeType;
+
 public interface ChallengeRoomRepository extends JpaRepository<ChallengeRoom, Long> {
+	// PESSIMISTIC_WRITE 락을 사용하여 해당 챌린지룸을 수정할 때 다른 트랜잭션이 접근하지 못하도록 함
+	// 유저 참가시에 정원 초과를 방지하기 위해 사용, 단순 조회(목록 등)에는 절대 쓰지 말것
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
+	@Query("SELECT c FROM ChallengeRoom c WHERE c.id = :id")
+	Optional<ChallengeRoom> findByIdWithLock(@Param("id") Long id);
 }
