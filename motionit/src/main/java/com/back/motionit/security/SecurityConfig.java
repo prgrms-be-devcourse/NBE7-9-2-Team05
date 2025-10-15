ad85b0d037479fe456f1c9138b145e0c754fe196
minibr <minimin9823@gmail.com>
2025-10-15 14:36:24 +0900

    feat: JWT Dto 추가 및 Security 구조 변경

--- motionit/src/main/java/com/back/motionit/security/SecurityConfig.java
+++ motionit/src/main/java/com/back/motionit/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.back.motionit.global.security;
+package com.back.motionit.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
