24316e6d92507ad567597980b06617ef3f711a8d
minibr <minimin9823@gmail.com>
2025-10-15 15:32:56 +0900

    feat: 일반 로그인 DTO 추가

---
+++ motionit/src/main/java/com/back/motionit/domain/auth/dto/AuthResponse.java
@@ -0,0 +1,17 @@
+package com.back.motionit.domain.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+	private String accessToken;
+	private Long userId;
+	private String email;
+	private String nickname;
+}
