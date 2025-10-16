f0b2c2c10532f721c09f26c50cd223317b817d07
gksdud1109 <vectorh532@gmail.com>
2025-10-16 20:27:39 +0900

    refactor: 공통응답코드 적용, swagger문서 설정 적용

--- motionit/src/main/java/com/back/motionit/domain/challenge/participant/service/ChallengeParticipantService.java
+++ motionit/src/main/java/com/back/motionit/domain/challenge/participant/service/ChallengeParticipantService.java
@@ -10,30 +10,37 @@ import com.back.motionit.domain.challenge.room.entity.ChallengeRoom;
 import com.back.motionit.domain.challenge.room.repository.ChallengeRoomRepository;
 import com.back.motionit.domain.user.entity.User;
 import com.back.motionit.domain.user.repository.UserRepository;
-import com.back.motionit.global.exception.ServiceException;
+import com.back.motionit.global.error.code.ChallengeParticipantErrorCode;
+import com.back.motionit.global.error.exception.BusinessException;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class ChallengeParticipantService {
 
 	private final ChallengeParticipantRepository challengeParticipantRepository;
 	private final ChallengeRoomRepository challengeRoomRepository;
 	private final UserRepository userRepository;
 
+	@Transactional
 	public void joinChallengeRoom(Long userId, Long roomId) {
 		User user = userRepository.findById(userId)
-			.orElseThrow(() -> new ServiceException("400", "유저를 찾을 수 없습니다"));
+			.orElseThrow(() -> new BusinessException(ChallengeParticipantErrorCode.NOT_FOUND_USER));
 
 		ChallengeRoom challengeRoom = challengeRoomRepository.findByIdWithLock(roomId)
-			.orElseThrow(() -> new ServiceException("400", "챌린지 룸을 찾을 수 없습니다"));
+			.orElseThrow(() -> new BusinessException(ChallengeParticipantErrorCode.CANNOT_FIND_CHALLENGE_ROOM));
 
-		// 정원 확인(lock 적용)
-		Integer currentParticipants = challengeParticipantRepository.countByChallengeRoom(challengeRoom);
+		boolean alreadyJoined = challengeParticipantRepository.existsByUserAndChallengeRoom(user, challengeRoom);
+		if (alreadyJoined) {
+			throw new BusinessException(ChallengeParticipantErrorCode.ALREADY_JOINED);
+		}
+
+		Integer currentParticipants = challengeParticipantRepository.countByChallengeRoomAndQuitedFalse(challengeRoom);
+
+		// 챌린지 룸의 현재 참가자 수가 최대 인원 수에 도달했는지 확인
 		if (currentParticipants >= challengeRoom.getCapacity()) {
-			throw new ServiceException("400", "챌린지 참가 인원이 초과되었습니다");
+			throw new BusinessException(ChallengeParticipantErrorCode.FULL_JOINED_ROOM);
 		}
 
 		ChallengeParticipant participant = new ChallengeParticipant(user, challengeRoom,
@@ -41,19 +48,20 @@ public class ChallengeParticipantService {
 		challengeParticipantRepository.save(participant);
 	}
 
+	@Transactional
 	public void leaveChallenge(Long userId, Long challengeRoomId) {
 
 		User user = userRepository.findById(userId)
-			.orElseThrow(() -> new ServiceException("400", "챌린지 룸에 존재하지 않는 유저입니다"));
+			.orElseThrow(() -> new BusinessException(ChallengeParticipantErrorCode.NOT_FOUND_USER));
 
 		ChallengeRoom challengeRoom = challengeRoomRepository.findById(challengeRoomId)
-			.orElseThrow(() -> new ServiceException("400", "챌린지 룸을 찾을 수 없습니다"));
+			.orElseThrow(() -> new BusinessException(ChallengeParticipantErrorCode.CANNOT_FIND_CHALLENGE_ROOM));
 
 		ChallengeParticipant participant = challengeParticipantRepository
 			.findByUserAndChallengeRoom(user, challengeRoom)
-			.orElseThrow(() -> new ServiceException("400", "참가자가 아닙니다"));
+			.orElseThrow(() -> new BusinessException(ChallengeParticipantErrorCode.NO_PARTICIPANT_IN_ROOM));
 
-		// Soft delete (isActive=false)
+		// Soft delete
 		participant.quitChallenge();
 	}
 }
