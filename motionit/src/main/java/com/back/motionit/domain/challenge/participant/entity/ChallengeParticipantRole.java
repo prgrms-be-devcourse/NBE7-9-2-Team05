446fca731c2bfdf3a08806a49759c60a7e8cfd59
gksdud1109 <vectorh532@gmail.com>
2025-10-17 15:50:31 +0900

    refactor: 방참여 서비스 로직 리팩터링, 운동방 생성시 방장권한으로 자동참여, 해당 기능 테스트 코드 작성

--- motionit/src/main/java/com/back/motionit/domain/challenge/participant/entity/ChallengeParticipantRole.java
+++ motionit/src/main/java/com/back/motionit/domain/challenge/participant/entity/ChallengeParticipantRole.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ChallengeParticipantRole {
-	ADMIN("admin"),
+	HOST("host"),
 	NORMAL("normal");
 
 	private final String value;
