1e38040600b59168863788f4a719736cd0b10971
heygeeji <heygeeji@gmail.com>
2025-10-20 03:02:11 +0900

    feat: CustomAuthenticationFilter 삽입 및 CORS 설정 추가

--- motionit/src/main/java/com/back/motionit/security/config/SecurityConfig.java
+++ motionit/src/main/java/com/back/motionit/security/config/SecurityConfig.java
@@ -1,13 +1,19 @@
 package com.back.motionit.security.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import com.back.motionit.security.CustomAuthenticationFilter;
 import com.back.motionit.security.handler.CustomOAuth2LoginSuccessHandler;
 
 import lombok.RequiredArgsConstructor;
@@ -18,6 +24,7 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfig {
 
 	private final CustomOAuth2LoginSuccessHandler customOAuth2LoginSuccessHandler;
+	private final CustomAuthenticationFilter customAuthenticationFilter;
 
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -32,11 +39,29 @@ public class SecurityConfig {
 			.headers((headers) -> headers
 				.addHeaderWriter(new XFrameOptionsHeaderWriter(
 					XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
+			.addFilterBefore(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
 			.sessionManagement(sessionManagement ->
 				sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.oauth2Login(oauth2 -> {
 				oauth2.successHandler(customOAuth2LoginSuccessHandler);
 			});
 		return http.build();
+	}
+
+	@Bean
+	public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+
+		configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+
+		configuration.setAllowedHeaders(List.of("*"));
+
+		configuration.setAllowCredentials(true);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/api/**", configuration);
+
+		return source;
 	}
 }
