78f9a9a8509b5456657b1eb40f4a34cf2f739c79
gksdud1109 <vectorh532@gmail.com>
2025-10-23 01:25:41 +0900

    feat: 댓글 API 프론트 반영, 인증인가-응답구조 일부 개선

--- frontend/motionit/src/components/index.tsx
+++ frontend/motionit/src/components/index.tsx
@@ -1,2 +1,3 @@
 export { default as UploadVideoForm } from "../components/UploadVideoForm";
 export { default as VideoItem } from "../components/VideoItem";
+export { default as CommentSection } from "../components/CommentSection";
