9820f4164e6509376235973e0de130e4b5d69c4e
minibr <minimin9823@gmail.com>
2025-10-23 11:57:47 +0900

    fix: 프로필 이미지 URL 생성 로직 수정
    - 외부 URL(http/https)은 CDN Sign 건너뛰고 그대로 반환
    - S3 ObjectKey는 CloudFront Signed URL 적용
    - 기본 프로필 이미지를 S3 이미지로 변경 (images/profiles/kakao-default-profile.png)

--- motionit/src/main/java/com/back/motionit/global/constants/ProfileImageConstants.java
+++ motionit/src/main/java/com/back/motionit/global/constants/ProfileImageConstants.java
@@ -3,7 +3,7 @@ package com.back.motionit.global.constants;
 public final class ProfileImageConstants {
 
 	public static final String DEFAULT_PROFILE_IMAGE =
-		"https://ui-avatars.com/api/?name=User&background=4F46E5&color=fff";
+		"images/profiles/kakao-default-profile.png";
 
 	private ProfileImageConstants() {
 	}
