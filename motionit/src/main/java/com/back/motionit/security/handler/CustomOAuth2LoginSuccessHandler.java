9af3425ec57995bb171865a73b3499a09ea7d3a1
minibr <minimin9823@gmail.com>
2025-10-24 15:31:08 +0900

    fix: RequestContext 의존성 제거

--- motionit/src/main/java/com/back/motionit/security/handler/CustomOAuth2LoginSuccessHandler.java
+++ motionit/src/main/java/com/back/motionit/security/handler/CustomOAuth2LoginSuccessHandler.java
@@ -9,8 +9,8 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 import org.springframework.stereotype.Component;
 
 import com.back.motionit.domain.auth.social.service.SocialAuthService;
-import com.back.motionit.domain.user.entity.User;
 import com.back.motionit.global.request.RequestContext;
+import com.back.motionit.security.SecurityUser;
 import com.back.motionit.security.jwt.JwtTokenDto;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -28,8 +28,10 @@ public class CustomOAuth2LoginSuccessHandler implements AuthenticationSuccessHan
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 		Authentication authentication) throws IOException {
 
-		User user = requestContext.getActor();
-		JwtTokenDto tokens = socialAuthService.generateTokensById(user.getId());
+		SecurityUser securityUser = (SecurityUser)authentication.getPrincipal();
+		Long userId = securityUser.getId();
+
+		JwtTokenDto tokens = socialAuthService.generateTokensById(userId);
 
 		requestContext.setCookie("accessToken", tokens.getAccessToken());
 		requestContext.setCookie("refreshToken", tokens.getRefreshToken());
