8671146af54c0ff0c4a7b145a9d37103dd90ddc5
SicpamaMinwoo <minwoo@sicpama.com>
2025-10-31 17:07:34 +0900

    chore: 깃헙 커버리지 및 CI 설정파일 추가

--- motionit/src/test/java/com/back/motionit/domain/challenge/participant/controller/ChallengeParticipantControllerTest.java
+++ motionit/src/test/java/com/back/motionit/domain/challenge/participant/controller/ChallengeParticipantControllerTest.java
@@ -10,9 +10,11 @@ import java.util.Optional;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -38,6 +40,8 @@ import com.back.motionit.security.SecurityUser;
 @SpringBootTest
 @ActiveProfiles("test")
 @AutoConfigureMockMvc(addFilters = false)
+@WebMvcTest(ChallengeParticipantController.class)
+@Tag("integration")
 @Transactional
 public class ChallengeParticipantControllerTest {
 
--- motionit/src/test/java/com/back/motionit/domain/challenge/video/controller/ChallengeVideoControllerTest.java
+++ motionit/src/test/java/com/back/motionit/domain/challenge/video/controller/ChallengeVideoControllerTest.java
@@ -8,9 +8,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -40,6 +42,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @SpringBootTest
 @ActiveProfiles("test")
 @AutoConfigureMockMvc(addFilters = false)
+@WebMvcTest(ChallengeVideoController.class)
+@Tag("integration")
 @Transactional
 class ChallengeVideoControllerTest {
 
